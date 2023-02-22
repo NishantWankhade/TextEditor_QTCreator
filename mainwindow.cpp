ffcfe508df59bad20c73b11e467f2f0df567c1f7
NGW334 <ngw334@gmail.com>
2023-02-22 07:20:52 +0530

    Adding functionalities to the menu items

---
+++ mainwindow.cpp
@@ -0,0 +1,80 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_actionNew_triggered()
+{
+    mFilename = "";
+    ui->textEdit->setPlainText("");
+
+}
+
+
+
+
+void MainWindow::on_actionOpen_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionSave_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionSave_As_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionCopy_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionCut_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionPaste_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionUndo_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionRedo_triggered()
+{
+
+}
+
+
+void MainWindow::on_actionBold_triggered()
+{
+
+}
+
